870f5970c709bdd4700443f40f9ccca317da7e70
Lev1reG <derentanaphan@mail.ugm.ac.id>
2024-11-04 22:44:56 +0700

    feat: using module to express app

--- src/index.js
+++ src/index.js
@@ -1,11 +1,32 @@
 const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const process = require("process");
 
 const app = express();
 
+// DOTENV CONFIG
+dotenv.config();
+
+// MIDDLEWARE
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(morgan("dev"));
+
+// CORS
+app.use(cors());
+
+// ROUTES
 app.get("/", (req, res) => {
   res.send("Welcome to PAW 10 Backend Service");
 });
 
+// APP LISTEN
 app.listen(5000, () => {
   console.log("Server is running on http://localhost:5000");
 });
