96b92015abd066a2184385e798ab9aa1ddfd1764
Lev1reG <derentanaphan@mail.ugm.ac.id>
2024-11-08 14:19:21 +0700

    feat: add ensure admin middleware

---
+++ src/middlewares/authentication.js
@@ -0,0 +1,35 @@
+const { decodeSessionJwt } = require("../helpers/authentication");
+const { getUserBySessionToken } = require("../models/User");
+
+const ensureAdmin = async (req, res, next) => {
+  try {
+    const decodedSession = decodeSessionJwt(req, res);
+
+    if (decodedSession.role !== "admin") {
+      return res.status(401).json({
+        message: "Unauthorized",
+      });
+    }
+
+    const existingUser = await getUserBySessionToken(
+      decodedSession.sessionToken,
+    );
+
+    if (!existingUser) {
+      return res.status(401).json({
+        message: "Unauthorized",
+      });
+    }
+
+    next();
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+module.exports = {
+  ensureAdmin,
+};
