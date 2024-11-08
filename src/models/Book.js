7c45a02ff00de1fbb61a91bccfe296f70b88cc83
Lev1reG <derentanaphan@mail.ugm.ac.id>
2024-11-08 14:13:21 +0700

    feat: add book model

---
+++ src/models/Book.js
@@ -0,0 +1,69 @@
+const mongoose = require("mongoose");
+
+const BookSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  authors: [
+    {
+      type: String,
+      required: true,
+    },
+  ],
+  isbn: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  publisher: String,
+  year: Number,
+  genres: [String],
+  description: String,
+  language: String,
+  coverImageUrl: String,
+  stock: {
+    type: Number,
+    required: true,
+  },
+});
+
+const BookModel = mongoose.model("Book", BookSchema);
+
+const getBooks = () => BookModel.find();
+
+const getBookByIsbn = (isbn) => BookModel.findOne({ isbn });
+
+const getBookById = (id) => BookModel.findById(id);
+
+const updateBookById = (id, values) =>
+  BookModel.findByIdAndUpdate(id, values, {
+    new: true,
+    runValidators: true,
+  });
+
+const createBook = (values) =>
+  new BookModel(values).save().then((book) => book.toObject());
+
+const deleteBookById = (id) => BookModel.findOneAndDelete({ _id: id });
+
+const getBooksByAuthor = (author) =>
+  BookModel.find({ authors: { $regex: new RegExp(author, i) } });
+
+const getBooksByGenre = (genre) =>
+  BookModel.find({ genres: { $regex: new RegExp(genre, i) } });
+
+const getBooksByYear = (year) => BookModel.find({ year });
+
+module.exports = {
+  getBooks,
+  getBookByIsbn,
+  getBookById,
+  updateBookById,
+  createBook,
+  deleteBookById,
+  getBooks,
+  getBooksByAuthor,
+  getBooksByGenre,
+  getBooksByYear,
+};
